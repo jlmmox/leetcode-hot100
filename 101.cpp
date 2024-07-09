15bdb6cebab6143953b0dbf915715b97c193d7c6
jlmmox <745482964m@gmail.com>
2024-07-09 12:58:01 +0800

    Update 101.cpp

--- 101.cpp
+++ 101.cpp
@@ -19,7 +19,13 @@ struct TreeNode {
  };
 class Solution {
 public:
+    bool check(TreeNode *p, TreeNode *q) {
+        if (!p && !q) return true;//两个都为空，则为镜像
+        if (!p || !q) return false;//有一个不为空，则不为镜像
+        return p->val == q->val && check(p->left, q->right) && check(p->right, q->left);//带入只有一个左右子树的二叉树中验证
+    }
+
     bool isSymmetric(TreeNode* root) {
-        
+        return check(root, root);
     }
 };
